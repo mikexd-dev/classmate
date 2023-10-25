0915febbb02261846aca03dd829aa771171b00aa
Mike Zhang <mikexd.dev@gmail.com>
2023-10-25 18:20:46 +0800

    created main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,115 +1,134 @@
 "use client";
 
+import { useWalletAuth } from "@/modules/wallet/hooks/useWalletAuth";
+import ConnectWallet from "@/components/ConnectWallet";
+import { signIn, useSession } from "next-auth/react";
+import TypewriterTitle from "@/components/generic/TypeWriter";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Loader2 } from "lucide-react";
+import Link from "next/link";
+import { DialogHeader } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from "@radix-ui/react-dialog";
+import CreateNoteDialog from "@/components/course/NoteDialog";
+import CreateWalletDialog from "@/components/generic/CreateWalletDialog";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import ConfettiComponent from "@/components/Confetti";
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 
 export default function Home() {
+  const { isConnecting, isConnected, connect, connectionError, wallet } =
+    useWalletAuth();
+  const { data: session, status } = useSession();
+  const [open, setOpen] = React.useState(false);
+  const [newUser, setNewUser] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  useEffect(() => {
+    if (
+      session &&
+      (session?.user?.address == null || session?.user?.address == "")
+    ) {
+      setOpen(true);
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (session?.user?.address) {
+      setNewUser(true);
+      // router.push("/gallery" + "?" + createQueryString("new", "true"));
+    }
+  }, [session?.user?.address]);
+
+  const router = useRouter();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="bg-gradient-to-r min-h-screen  from-rose-100 to-teal-100">
+      {newUser && open && <ConfettiComponent />}
+      <CreateWalletDialog open={open} setOpen={setOpen} newUser={newUser} />
+
+      <AnimatePresence>
+        <motion.div
+          className="flex justify-center items-center min-h-screen flex-col z-20"
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: {
+              duration: 1,
+              ease: [0.5, 0.71, 1, 1.5],
+              delayChildren: 0.5,
+              staggerChildren: 0.5,
+            },
+          }}
+        >
+          <Image
+            src="/roboto.svg"
+            width={200}
+            height={200}
+            alt="Hero Image"
+            className="absolute inset-0 z-10  left-[20%] top-[20%] sm:h-[200px] sm:w-[200px] sm:left-[30%] sm:top-[20%] md:h-[300px] md:w-[300px] md:left-[0%] lg:left-[20%] lg:top-[25%]"
+          />
+          <div className="border border-black rounded-md p-2 text-xs mb-5">
+            Best viewed on desktop*
+          </div>
+          <motion.div
+            className="font-light text-8xl flex flex-row"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <motion.div className="font-medium">AI</motion.div>ducation
+          </motion.div>
+          <motion.div
+            className="font-normal text-lg pb-5"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            The future is yours. Supercharge your learning on-chain.
+          </motion.div>
+          {/* <Image
+            src="/pondering.png"
+            width={500}
+            height={500}
+            alt="Hero Image"
+            className="absolute inset-0 left-[15%] top-[20%] -z-2"
+          /> */}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            {session?.user?.address ? (
+              <Button
+                className=""
+                onClick={() => {
+                  router.push("/dashboard");
+                }}
+              >
+                {`Let's Go! ${session?.user?.name}`}
+                <ArrowRight className="ml-2 w-5 h-5" strokeWidth={3} />
+              </Button>
+            ) : (
+              <Button
+                className=""
+                onClick={() => {
+                  setIsLoading(true);
+                  signIn("google");
+                }}
+              >
+                {isLoading ? "loading..." : "Get Started"}
+                {isLoading ? (
+                  <Loader2 className="w-4 h-4 mr-1 animate-spin" />
+                ) : (
+                  <ArrowRight className="ml-2 w-5 h-5" strokeWidth={3} />
+                )}
+              </Button>
+            )}
+          </motion.div>
+        </motion.div>
+      </AnimatePresence>
+    </div>
   );
 }
